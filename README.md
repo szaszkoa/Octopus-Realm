350d9523d55b7aa925fabce43d313326d7513820
Szaszko <szaszkoa@gmail.com>
2019-10-15 18:37:54 +0200

    trying to compile ts to work

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Octopus-Realm
 My first attempt at a personal project, meaning horrible spaghetti code awaits if you take a look at it.
+
+
+# Description
+
+Since i like incremental games, i had the idea to create a basic game to practice TS, HTML and CSS, as my first personal project. So here it goes.
--- data/main.ts
+++ data/main.ts
@@ -1,1 +1,13 @@
 'use strict';
+
+let gameData: any = {
+  shellfish: 0,
+  shellfishPerClick: 1
+}
+
+// octopi hunting for shellfish
+
+function octopusHunt(): void {
+  gameData.shellfish += gameData.shellfishPerClick;
+  document.getElementById('shelllfishAvailable').innerHTML = `${gameData.shellfish} shellfish collected`
+}
