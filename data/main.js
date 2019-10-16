17220d2d3d486b0139fcbeadc0af0b438caf5317
Szaszko <szaszkoa@gmail.com>
2019-10-16 11:50:55 +0200

    work star commit, and pushing the progress i made yesterday

--- data/main.js
+++ data/main.js
@@ -1,10 +1,31 @@
 'use strict';
 var gameData = {
     shellfish: 0,
-    shellfishPerClick: 1
+    shellfishPerClick: 10 // set to 10 to avoid breaking fingers when testing. original value is 1
+};
+var resourceModifiers = {
+    // shellfish per click modifier
+    octopusPopulation: 1
 };
 // octopi hunting for shellfish
-function octopusHunt() {
-    gameData.shellfish += gameData.shellfishPerClick;
-    document.getElementById('shellfishAvailable').innerHTML = gameData.shellfish + " shellfish collected";
+function sendOctopusToHunt() {
+    gameData.shellfish += resourceModifiers.octopusPopulation == 1 ? gameData.shellfishPerClick : gameData.shellfishPerClick * resourceModifiers.octopusPopulation;
+    document.getElementById('shellfishAvailable').innerHTML = gameData.shellfish + " shellfish collected.";
 }
+// breeding octopi
+function breedOctopus() {
+    var priceOfOctopus = 100;
+    if (gameData.shellfish >= priceOfOctopus) {
+        resourceModifiers.octopusPopulation += 1;
+        gameData.shellfish -= priceOfOctopus;
+        document.getElementById('shellfishAvailable').innerHTML = gameData.shellfish + " shellfish collected.";
+        document.getElementById('octopusPopulation').innerHTML = resourceModifiers.octopusPopulation + " Octopi in you Realm.";
+    }
+    else {
+        console.log('You cant afford to breed octopi');
+    }
+}
+// automatic resource generation
+var mainGameLoop = window.setInterval(function () {
+    resourceModifiers.octopusPopulation == 1 ? undefined : sendOctopusToHunt();
+}, 1000);
--- data/main.ts
+++ data/main.ts
@@ -2,12 +2,53 @@
 
 let gameData: any = {
   shellfish: 0,
-  shellfishPerClick: 1
+  shellfishPerClick: 10 // set to 10 to avoid breaking fingers when testing. original value is 1
+
 }
+
+let resourceModifiers: any = {
+  // shellfish per click modifier
+  octopusPopulation: 1
+
+}
+
 
 // octopi hunting for shellfish
-
-function octopusHunt(): void {
-  gameData.shellfish += gameData.shellfishPerClick;
-  document.getElementById('shellfishAvailable').innerHTML = `${gameData.shellfish} shellfish collected`
+function sendOctopusToHunt(): void {
+  gameData.shellfish += resourceModifiers.octopusPopulation == 1 ? gameData.shellfishPerClick : gameData.shellfishPerClick * resourceModifiers.octopusPopulation;
+  document.getElementById('shellfishAvailable').innerHTML = `${gameData.shellfish} shellfish collected.`
 }
+
+// breeding octopi
+function breedOctopus(): any {
+  let priceOfOctopus = 100;
+  if (gameData.shellfish >= priceOfOctopus) {
+    resourceModifiers.octopusPopulation += 1;
+    gameData.shellfish -= priceOfOctopus;
+    document.getElementById('shellfishAvailable').innerHTML = `${gameData.shellfish} shellfish collected.`
+    document.getElementById('octopusPopulation').innerHTML = `${resourceModifiers.octopusPopulation} Octopi in you Realm.`
+  } else {
+    console.log('You cant afford to breed octopi');
+  }
+}
+
+// automatic resource generation
+let mainGameLoop: any = window.setInterval(() => {
+  resourceModifiers.octopusPopulation == 1 ? undefined : sendOctopusToHunt()
+}, 1000)
+
+/** saving game to be added later
+
+//game loading 
+let saveGame: object = JSON.parse(localStorage.getItem('octopusRealm'))
+if (saveGame != null) {
+  gameData = saveGame;
+
+  // game saving loop
+  let saveGameLoop: any = window.setInterval(() => {
+    localStorage.setItem('octopusRealm', JSON.stringify(gameData));
+  }, 15000)
+
+}
+
+*/
